8a39dcf30298ebf4f13d5c9c2f9d987022aaebc6
Zac Smith <zrs3141592@gmail.com>
2020-07-31 19:17:00 -0600

    add twitter basics to create post

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,24 @@
 const Twitter = require('twitter')
 const CronJob = require('cron').CronJob
 
+// const TwitterFeed = async () => {
+//   let twitter_config = {
+//     consumer_key: process.env.CONSUMER_KEY,
+//     consumer_secret: process.env.CONSUMER_SECRET_KEY,
+//     access_token_key: process.env.ACCESS_TOKEN,
+//     access_token_secret: process.send.ACCESS_TOKEN_SECRET,
+//   }
+//   try {
+//     let client = new Twitter(twitter_config)
+//     let status = 'Hello Twitter, posting from NodeJS with my first Cronjob app!'
+//     let d = await client.post('statuses/update', { status })
+//     return d
+//   } catch (error) {
+//     console.log(`Error: ${error}`)
+//     return
+//   }
+// }
+
 const consoleTest = async () => {
   try {
     let now = new Date()
